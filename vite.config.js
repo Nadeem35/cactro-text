12774f6d9a7972a121a98250dd14f58e588e7f4f
nadeem35 <nadeem.cse72@gmail.com>
2026-05-03 12:47:19 +0530

    fix repo name

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/cactro-test/", // 👈 IMPORTANT (repo name)
+  base: "/cactro-text/", // 👈 IMPORTANT (repo name)
 });
